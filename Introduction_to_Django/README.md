52150dbbfdd6888d4ba9cf87440751bd4a057d9d
Malloy-Lucio <mltechguides@gmail.com>
2025-11-22 11:50:41 +0000

    added views

--- Introduction_to_Django/README.md
+++
@@ -1,9 +0,0 @@
-# LibraryProject
-
-This is a basic Django project created for learning purposes.
-
-## Setup Instructions
-
-1. Install Django:
-   ```bash
-   pip install django
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from django.views.generic.detail import DetailView
 from .models import Book
 from .models import Library
+from django.contrib.auth.models import login, logout
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import reverse_lazy
 
 # Create your views here.
 #function-based view to list all books
@@ -15,3 +20,27 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+class LoginUserView(LoginView):
+    template_name = 'relationship_app/login.html'
+    form_class = AuthenticationForm
+    success_url = reverse_lazy('list_books')
+
+def get_success_url(self):
+    return self.success_url
+
+def logout_view(request):
+    logout(request)
+    return render(request, 'relationship_app/logout.html')
+
+def register_view(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Log user in after registration
+            return redirect("list_books")
+    else:
+        form = UserCreationForm()
+
+    return render(request, "relationship_app/register.html", {"form": form})
